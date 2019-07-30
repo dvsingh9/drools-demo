313b1a73e85f8def855f9e44f34f117725c76855
DV Singh <dvsingh@protonmail.com>
2019-07-30 23:13:18 +0530

    update

--- src/main/java/me/dvsingh/drools/DroolDemoApplication.java
+++ src/main/java/me/dvsingh/drools/DroolDemoApplication.java
@@ -4,12 +4,16 @@ import me.dvsingh.drools.model.Fare;
 import me.dvsingh.drools.model.TaxiRide;
 import me.dvsingh.drools.service.TaxiFareConfiguration;
 import me.dvsingh.drools.service.TaxiService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class DroolDemoApplication {
+@SpringBootApplication
+public class DroolDemoApplication  implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    public void run(String... args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(TaxiFareConfiguration.class);
         TaxiService taxiFareCalculatorService = (TaxiService) context.getBean(TaxiService.class);
         TaxiRide taxiRide = new TaxiRide();
@@ -17,6 +21,11 @@ public class DroolDemoApplication {
         taxiRide.setDistanceInMile(90L);
         Fare rideFare = new Fare();
         taxiFareCalculatorService.calculateFare(taxiRide, rideFare);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolDemoApplication.class, args);
+
     }
 
 }
